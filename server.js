1ddf4dca0766ed6fbb748e22957c35c8e08dc6c3
Joeeazy <joeeazy34@gmail.com>
2024-04-10 17:50:01 +0300

    use model to save data in MongoDB

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require("express");
 
 const dotenv = require("dotenv");
 
+const Product = require("./models/productModel");
+
 //create a mongoose variable
 const mongoose = require("mongoose");
 
 //initialize an app using
 const app = express();
+
+//middleware
+app.use(express.json());
 
 //config dotenv to use it as process.env for environmental varables
 dotenv.config();
@@ -24,6 +29,19 @@ app.get("/", (req, res) => {
 app.get("/blog", (req, res) => {
   //send something to client
   res.send("Hello Blog, My Name Is Joe");
+});
+
+//route for saving data into the database
+app.post("/product", async (req, res) => {
+  try {
+    //create a new product in the db
+    const product = await Product.create(req.body);
+    //respond with data
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 //connect to you mongodb then console if connected or not
